10ab75875482389ff945db257ebef10108a106f7
Ranger md <153409985+Marksimiyu@users.noreply.github.com>
2025-04-04 14:02:20 +0300

    Update qr.js

--- qr.js
+++ qr.js
@@ -17,13 +17,13 @@ const { Boom } = require("@hapi/boom");
 const PORT = process.env.PORT ||  5000
 const MESSAGE = process.env.MESSAGE ||  `
 ┌───⭓『
-❒ *WASI-MD*
+❒ *DEVSPACE*
 ❒ _NOW DEPLOY IT_
 └────────────⭓
 ┌───⭓
 ❒  • Chat with owner •
 ❒ *GitHub:* __https://github.com/WASI-MD_
-❒ *Author:* _wa.me/923192173398_
+❒ *Author:* _wa.me/254110550356_
 ❒ *YT:* _https://youtube.com/@wasitech10_
 └────────────⭓
 `
